995ba79b9014f5add19686dc2aaa98783469cc00
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-19 14:48:44 -0800

    hello ,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,6 @@
 /*This code prints some text*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art\,");
 	return (0);
 }

ebd27bbd10c770c3fc7bfbbbdde754d049b0aa78
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-20 13:14:45 -0800

    Tsk 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  * main - prints to string
- *   * Return: 1
- *    */
+ * main - prints to string
+ * Return: 1
+ */
 int main(void)
 {
-	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }

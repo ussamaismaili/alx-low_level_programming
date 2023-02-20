ac0707b1ed7c033dca26ed0d4e9354843b50b08b
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-20 13:05:16 -0800

    tsk 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints to string
- * Description: Prints "and that piece of art is useful.." without puts
- * Return: 1
- */
+ *  * main - prints to string
+ *   * Return: 1
+ *    */
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long l = 59;
-	long fd = 1;
-	long syscall = 1;
-	long ret = 0;
-	__asm__("syscall"
-			: "=a" (ret)
-			: "a" (syscall),
-			"D" (fd),
-			"S" (s),
-			"d" (l));
+	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }

83238dd06d005bab07802dd8751876e96fbf91d0
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-21 14:19:27 -0800

    Alphabet printed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - function that print alphabets in lower case
+ * Return: ofc (0)
+ */
+void print_alphabet(void)
+{	char i;
+	for (i = 'a ', i => 'z', i++)
+		_putchar(i);
+		_putchar('\n');
+}

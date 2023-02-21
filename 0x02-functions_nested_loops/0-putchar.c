baf603840f99dc9b5b4c43d0b7ae94d10b0d0349
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-21 13:56:35 -0800

    print 'putchar 'done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,9 +11,9 @@ int main(void)
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-	_putchar('h);
+	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar(\n);
-return (0)
+	_putchar('\n');
+return (0);
 }

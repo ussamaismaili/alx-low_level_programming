bb9be433a7f7950fec3667a4bccb13e1d8f11752
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-21 18:40:50 -0800

    done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower- checks for weather is c lowercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ * Return _islower : (1) if true and (0) if false
+ */
+
+int _islower(int c)
+{
+	if (c == 97)
+
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

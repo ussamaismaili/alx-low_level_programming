20d512286d89be85a7e14cbfd088051ff4201025
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-22 07:58:25 -0800

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value.
+ *
+ * @num: is the given number
+ *
+ * Return: (num) if positive number , and (-num) if negative
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{	return num = -num;
+	}
+	else
+	{	return num;
+	}
+}

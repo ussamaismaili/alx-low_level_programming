51b0f66f5175abac03c09a1a886242b6c98e7c87
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-21 17:52:53 -0800

    Alphabet printed X10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - function that print alphabets in lower case
+* Return: ofc (0)
+*/
+void print_alphabet_x10(void)
+{
+	char j;
+
+	while (j < 10)
+	{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	 _putchar('\n');
+	}
+	j++;
+	}
+}

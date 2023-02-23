11d69b4d3266d45c3332162a43bf7e9776d5489a
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-22 17:36:52 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - counts to 98
+ * @n: the goven number
+ * Return: the count
+ */
+void print_to_98(int n)
+{
+	if (n <= 98 || n < 0)
+	{
+	for (; n <= 98; n++)
+		printf("%4d, ", n);
+	}
+	else
+	{
+	for (; n >= 98; n--)
+		printf("%4d, ", n);
+	}
+}

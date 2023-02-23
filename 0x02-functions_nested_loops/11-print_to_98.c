59a6925ec906cadf9e720b81edfd855ccff59bd2
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-22 18:09:08 -0800

    98 Battery Street, the OG lfe9sa 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,13 +15,13 @@ void print_to_98(int n)
 	}
 	else  if (n == 98)
 	{
-		printf("%d, ", n);
+		printf("%d", n);
 		printf("\n");
 		}
 	else
 	{
 	for (; n >= 98; n--)
-		printf("%d", n);
+		printf("%d, ", n);
 	}
 	printf("\n");
 }

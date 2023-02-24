b9aba531cda40bb8db35b579ba9127d225a1f7df
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-23 17:14:39 -0800

    done

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or_negative - check numbers sign
+ * @i: passed number
+ * Return:(0)
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+	return (0);
+}

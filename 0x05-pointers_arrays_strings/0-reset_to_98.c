c1b2778db41d52211e350b36b950a4b655b54579
Oussama Ismaili <ismailiossama@gmail.com>
2023-02-27 18:41:46 -0800

    just started

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - change the value of the number
+ * @n: input
+ * Return: Always return 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+I am learning  arrays and pointers

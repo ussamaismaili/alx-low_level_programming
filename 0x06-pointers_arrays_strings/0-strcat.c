2e41d614a81d548f6e299f3cdda49b5731d66f16
Oussama Ismaili <ismailiossama@gmail.com>
2023-03-02 18:37:24 -0800

    done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - uhgrjehg dfgrdrg dfcdd
+ * @dest:khkshfr rrrr
+ * @src: fsrfrsf sfasef fs
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{	int i;
+	int j;
+	i = 0;
+	j = 0;
+	while (dest[i])
+	{	i++;
+	}
+	
+	while (src[j])
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+I am learning C and now we are practicing more pointers , arrays ,strings.

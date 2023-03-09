fe2144d8578b1e16b7c126f8c35d604a50293bc8
Oussama Ismaili <ismailiossama@gmail.com>
2023-03-09 12:43:18 -0800

    edits 1

---
+++ 0x08-recursion/0-puts_rescursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string with recusion
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (!(*s))
+	{
+	_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s +  1);
+}

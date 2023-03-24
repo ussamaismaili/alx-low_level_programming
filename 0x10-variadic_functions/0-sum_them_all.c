b91458a98b8e8438cd371fa88fccaba9033fe5c4
Oussama Ismaili <ismailiossama@gmail.com>
2023-03-23 19:56:50 -0700

    I am tired '-'

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list nums;
 	unsigned int index, sum = 0;
-
-	va_start(nums, n);
+	
 	if (n == 0)
 		return (0);
-
+	va_start(nums, n);
 	for (index = 0; index < n; index++)
 		sum += va_arg(nums, int);
 	va_end(nums);

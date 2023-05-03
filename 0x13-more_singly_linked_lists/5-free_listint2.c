87b76459091a93d776673825dd7ea1df2e42fb3c
Ismaili oussama <ismailiossama@gmail.com>
2023-05-02 19:56:47 -0700

    let's test 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_listint2 - free this list.
+ * @head: the list.
+ * Return: always void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+	listint_t *tr;
+
+	if (head)
+	{
+		tr = *head;
+		while (tr->next)
+		{
+			ptr = tr;
+			free(ptr);
+			tr = tr->next;
+		}
+		free(tr);
+		*head = NULL;
+	}
+}

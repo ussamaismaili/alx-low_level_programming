91682c0f34038268351c2ac0e6fe816261a7fdf5
ussamaismaili <ismailiossama@gmail.com>
2023-05-08 17:35:05 -0700

    tts

---
+++ ooi/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_bit - it sets the value of a bit to 1 at a given index.
+ * @n: the type unsigned long int input pointer.
+ * @index: a type unsigned int position of unit.
+ * Return: a 1 if works -1 if not.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n |= 1 << index;
+	return (1);
+
